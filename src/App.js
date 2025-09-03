3f8f81008c779f77b8628c93ec990f9bd684d56a
Andreas Geyer <andreasgeyer@MBP-von-Andreas.fritz.box>
2025-09-03 22:23:13 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <p>Current domain: <b>{window.location.hostname}</b></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

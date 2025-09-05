2389fafca940d3a846f4b966159092cea96dc637
Andreas Geyer <andreasgeyer@MBP-von-Andreas.fritz.box>
2025-09-05 17:14:45 +0200

    Added Rest Call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 
 import './App.css';
+import React from 'react';
+
 
 function App() {
+
+  const [message, setMessage] = React.useState('');
+
+  React.useEffect(() => {
+    const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:8080/hello';
+    fetch(apiUrl)
+      .then(response => response.text())
+      .then(data => setMessage(data))
+      .catch(error => setMessage('Error: ' + error.message));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
         <p>Current domain: <b>{window.location.hostname}</b></p>
+        
+        
+
+        <p>API response: <b>{message}</b></p>
       </header>
     </div>
   );
